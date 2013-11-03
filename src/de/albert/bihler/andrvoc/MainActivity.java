235e031c7b42853d49d15e16e9bf450f505b4388
Albert Bihler <bihleral@yahoo.com>
2013-11-03 18:58:36 +0100

    zwei Hardcodierte Fragen und eine Prüqung

--- src/de/albert/bihler/andrvoc/MainActivity.java
+++ src/de/albert/bihler/andrvoc/MainActivity.java
@@ -24,26 +24,26 @@ public class MainActivity extends Activity {
         Vokabel v= new Vokabel();
  
         
-     Spinner spinner = (Spinner) findViewById(R.id.planets_spinner);
-     // Create an ArrayAdapter using the string array and a default spinner layout
-     ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-    R.array.planets_array, android.R.layout.simple_spinner_item);
-    // adapter.add(new String("Test"));
-     //("dynamisch");
-     // Specify the layout to use when the list of choices appears
-     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-     // Apply the adapter to the spinner
-     spinner.setAdapter(adapter);
-        
-     Spinner s2 = (Spinner) findViewById(R.id.dates);
-     
-     
-     ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.date_array, android.R.layout.simple_spinner_item);
-     
-     //adapter2.add(new String("August"));
-     
-     adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-     s2.setAdapter(adapter2);
+//     Spinner spinner = (Spinner) findViewById(R.id.planets_spinner);
+//     // Create an ArrayAdapter using the string array and a default spinner layout
+//     ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+//    R.array.planets_array, android.R.layout.simple_spinner_item);
+//    // adapter.add(new String("Test"));
+//     //("dynamisch");
+//     // Specify the layout to use when the list of choices appears
+//     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//     // Apply the adapter to the spinner
+//     spinner.setAdapter(adapter);
+//        
+//     Spinner s2 = (Spinner) findViewById(R.id.dates);
+//     
+//     
+//     ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.date_array, android.R.layout.simple_spinner_item);
+//     
+//     //adapter2.add(new String("August"));
+//     
+//     adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//     s2.setAdapter(adapter2);
     }
 
     @Override
@@ -55,23 +55,23 @@ public class MainActivity extends Activity {
     
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.edit_message);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
     }
     
     /** Called when the user clicks the Neu button */
     public void neuButton(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        //EditText editText = (EditText) findViewById(R.id.edit_message);
-        Spinner planet =  (Spinner)findViewById(R.id.planets_spinner);
-//        planet.toString();
-        //.toString();
-        String message = "Hardcoded Text " + planet.getSelectedItem().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        //EditText editText = (EditText) findViewById(R.id.edit_message);
+//        Spinner planet =  (Spinner)findViewById(R.id.planets_spinner);
+////        planet.toString();
+//        //.toString();
+//        String message = "Hardcoded Text " + planet.getSelectedItem().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
     }    
     
     /** Called when the user clicks the start question button*/
--- src/de/albert/bihler/andrvoc/QuestionActivity.java
+++ src/de/albert/bihler/andrvoc/QuestionActivity.java
@@ -5,22 +5,30 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.ArrayAdapter;
 
 public class QuestionActivity extends Activity {
-
+//	private Spinner spinner findViewById(R.id.question_spinner_answer);
+	private TextView textWord;
+	private TextView textResult;
+	private Spinner answerSpinner;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_question);
-		
-		TextView textWord = (TextView) findViewById(R.id.question_field_word);
+
+		textWord = (TextView) findViewById(R.id.question_field_word);
         textWord.setText("morgens");
+        textResult = (TextView) findViewById(R.id.question_field_result);
+        textResult.setText("");
         
-        //TODO:answerSpinner über Klassen property ansprechen
-        Spinner answerSpinner = (Spinner) findViewById(R.id.question_spinner_answer);
+        //      spinner.setOnItemSelectedListener(new OnItemSelectedListener();
+        answerSpinner = (Spinner) findViewById(R.id.question_spinner_answer);
+                
         
         String array_spinner[]=new String[5];
         array_spinner[0]="at the morning";
@@ -33,8 +41,10 @@ public class QuestionActivity extends Activity {
         		R.layout.spinner_list, array_spinner);
         
         adapter.setDropDownViewResource(R.layout.spinner);
-        answerSpinner.setAdapter(adapter);        
+                
+        answerSpinner.setAdapter(adapter);
         
+        //answerSpinner.setOnItemSelectedListener(new OnItemSelectedListener());
 	}
 
 	@Override
@@ -48,18 +58,51 @@ public class QuestionActivity extends Activity {
 	public void doCheck(View view) {
 		Spinner answerSpinner = (Spinner) findViewById(R.id.question_spinner_answer);
 		String answer = answerSpinner.getSelectedItem().toString();
-		//TODO:result Text über Klassen property ansprechen
-	    if (answer.equals("in the morning")) {
-	    	TextView textResult = (TextView) findViewById(R.id.question_field_result);
+		if (answer.equals("in the morning")) {
+	    	textResult = (TextView) findViewById(R.id.question_field_result);
 	    	textResult.setTextColor(Color.GREEN);
 	        textResult.setText("Richtig!!!");
 	    }
 	    else {
-	    	TextView textResult = (TextView) findViewById(R.id.question_field_result);
+	    	textResult = (TextView) findViewById(R.id.question_field_result);
 	    	textResult.setTextColor(Color.RED);
 	        textResult.setText("Die Antwort ist leider falsch.");
 	    }
-	    	
 	}
+	
+	// Nächste Frage
+	public void doNext(View view) {
+		textWord.setText("Entschuldigen Sie bitte.");
+		clearResult();
+		
+		String array_spinner[]=new String[4];
+        array_spinner[0]="sorry";
+        array_spinner[1]="I'm sorry";
+        array_spinner[2]="excuse me";
+        array_spinner[3]="excuse please";
+//        array_spinner[4]="it's morning";
+        ArrayAdapter adapter = new ArrayAdapter(this,
+        		R.layout.spinner_list, array_spinner);
+        
+        adapter.setDropDownViewResource(R.layout.spinner);
+                
+        answerSpinner.setAdapter(adapter);
+        
+	}
+	
+	   private void clearResult()
+	   {
+		   textResult.setText("");
+	   }
+	
+//   public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+//	        // your code here
+//	    }
+
+//    @Override
+//    public void onNothingSelected(AdapterView<?> parentView) {
+//	        // your code here
+//	    }
+	
 	
 }
