ecdf4550851a087fc893303041ebd568ff1908f9
Robert Mathes <robert.d.mathes@gmail.com>
2013-11-08 23:50:35 +0100

    Spinner durch Radiobuttons ersetzt

--- src/de/albert/bihler/andrvoc/MainActivity.java
+++ src/de/albert/bihler/andrvoc/MainActivity.java
@@ -1,6 +1,5 @@
 package de.albert.bihler.andrvoc;
 
-import de.albert.bihler.andrvoc.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,74 +10,56 @@ import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
-	public final static String EXTRA_MESSAGE = "de.albert.bihler.MESSAGE";
-	private AppPreferences appPrefs;
-	private Spinner unitSpinner;
+    public final static String EXTRA_MESSAGE = "de.albert.bihler.MESSAGE";
+    private AppPreferences appPrefs;
+    private Spinner unitSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        init();
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
+
+	init();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        
-        return true;
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(R.menu.main, menu);
+
+	return true;
     }
-    
+
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
-//        Intent intent = new Intent(this, DisplayMessageActivity.class);
-//        EditText editText = (EditText) findViewById(R.id.edit_message);
-//        String message = editText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
-//        startActivity(intent);
     }
-    
+
     /** Called when the user clicks the Neu button */
     public void neuButton(View view) {
-//        Intent intent = new Intent(this, DisplayMessageActivity.class);
-//        //EditText editText = (EditText) findViewById(R.id.edit_message);
-//        Spinner planet =  (Spinner)findViewById(R.id.planets_spinner);
-////        planet.toString();
-//        //.toString();
-//        String message = "Hardcoded Text " + planet.getSelectedItem().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
-//        startActivity(intent);
-    }    
-    
-    /** Called when the user clicks the start question button*/
+    }
+
+    /** Called when the user clicks the start question button */
     public void startQuestion(View view) {
-//        Intent intent = new Intent(this, DisplayMessageActivity.class);
-//        String message = "Hier geht es jetzt mit dem Vokabeltrauning los…";
-//        intent.putExtra(EXTRA_MESSAGE, message);
-//        startActivity(intent);
-    	
-    	String unit = unitSpinner.getSelectedItem().toString();
-    	appPrefs.saveUnit(unit);
-    	    	
-    	Intent intent = new Intent(this, QuestionActivity.class);
-    	startActivity(intent);
-    }        
+
+	String unit = unitSpinner.getSelectedItem().toString();
+	appPrefs.saveUnit(unit);
+
+	Intent intent = new Intent(this, QuestionActivity.class);
+	startActivity(intent);
+    }
 
     // Zeugs initialisieren.
-    public void init(){
-    	
-    	unitSpinner = (Spinner) findViewById(R.id.main_spinner_unit);
-    	
-    	String array_spinner[]=new String[] {"benny_01", "en_unit00_01", "en_unit01_01", "en_unit01_02"};
-        ArrayAdapter<Object> adapter = new ArrayAdapter<Object>(this,
-        		R.layout.spinner_list, array_spinner);
-                adapter.setDropDownViewResource(R.layout.spinner);
-                
-        unitSpinner.setAdapter(adapter);
-    	
-    	appPrefs = new AppPreferences(getApplicationContext());
+    public void init() {
+
+	unitSpinner = (Spinner) findViewById(R.id.main_spinner_unit);
+
+	String array_spinner[] = new String[] { "benny_01", "en_unit00_01", "en_unit01_01", "en_unit01_02" };
+	ArrayAdapter<Object> adapter = new ArrayAdapter<Object>(this, R.layout.spinner_list, array_spinner);
+	adapter.setDropDownViewResource(R.layout.spinner);
+
+	unitSpinner.setAdapter(adapter);
+
+	appPrefs = new AppPreferences(getApplicationContext());
     }
-    
+
 }
--- src/de/albert/bihler/andrvoc/Vokabel.java
+++ src/de/albert/bihler/andrvoc/Vokabel.java
@@ -2,73 +2,32 @@ package de.albert.bihler.andrvoc;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
-
 
 public class Vokabel {
-	public String name;
-	public String translation;
-	public String alt_translation;
-	public List<String> altList;
-		
-	public Vokabel() {
-		name="excuse me";
-		translation="Entschuldigen Sie";
-		alt_translation="Entschuldigen Sie, bitte";
-	}
-	
-	public Vokabel(String string){
-		altList= new ArrayList<String>(Arrays.asList(string.split("#")));
-		translation=altList.get(0);
-		name=altList.get(1);
-		altList.remove(0);
-		randomizeList();
-	}
-			
-	public Vokabel(String v, String t) {
-		name=v;
-		translation=t;
-	}
-	
-	public Vokabel(String v, String t, String a) {
-		name=v;
-		translation=t;
-		alt_translation=a;
-	}
-	
-	public Vokabel (String v, String t, List<String> l)
-	{
-		name=v;
-		translation=t;
-		List<String> list = new ArrayList<String>();
-		list.addAll(l);
-		list.add(v);
-		altList = list;
-	}
-	
-	public Vokabel (List<String> l)
-	{
-		translation=l.get(0);
-		name=l.get(1);
-		l.remove(0);
-		List<String> list = new ArrayList<String>();
-		list.addAll(l);
-		altList = list;
-		randomizeList();
-	}
-	
-	public void randomizeList()
-	{
-		Random rand = new Random();
-	    for(int i = 1; i<= 10; i++)
-	    {
-	        int r1 = rand.nextInt(altList.size());
-	        int r2 = rand.nextInt(altList.size());
-	        String s1 = altList.get(r1);
-	        String s2 = altList.get(r2);
-	        altList.set(r1, s2);
-	        altList.set(r2, s1);
-	    }
-	}
+
+    private final String correctTranslation;
+    private final String originalWord;
+    private final List<String> alternativeTranslations;
+
+    public Vokabel(String string) {
+	alternativeTranslations = new ArrayList<String>(Arrays.asList(string.split("#")));
+	originalWord = alternativeTranslations.get(0);
+	correctTranslation = alternativeTranslations.get(1);
+	alternativeTranslations.remove(0);
+	Collections.shuffle(alternativeTranslations);
+    }
+
+    public String getOriginalWord() {
+	return originalWord;
+    }
+
+    public String getCorrectTranslation() {
+	return correctTranslation;
+    }
+
+    public List<String> getAlternativeTranslations() {
+	return alternativeTranslations;
+    }
 }
