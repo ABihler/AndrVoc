2c606fe23f9ddc8bc2b67e01a0cf969630fa35c9
Robert Mathes <robert.d.mathes@gmail.com>
2014-01-06 23:57:14 +0100

    Implemented text-to-speech in learning mode.
    Correct translations are now read in English, French and Spanish when the read icon is clicked.

--- src/de/robertmathes/android/orangeiron/LearningActivity.java
+++ src/de/robertmathes/android/orangeiron/LearningActivity.java
@@ -17,14 +17,17 @@
 package de.robertmathes.android.orangeiron;
 
 import java.util.Collections;
-import java.util.Currency;
 import java.util.List;
+import java.util.Locale;
 
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -32,7 +35,9 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 import de.robertmathes.android.orangeiron.db.DataSource;
 import de.robertmathes.android.orangeiron.model.Lesson;
 import de.robertmathes.android.orangeiron.model.Vokabel;
@@ -44,7 +49,7 @@ import de.robertmathes.android.orangeiron.model.Vokabel;
  * This sample shows an "info" action bar button that shows the back of a "card", rotating the front of the card out and the back of the card in. The reverse animation is played when the user presses the system Back button or the "photo" action bar button.
  * </p>
  */
-public class LearningActivity extends Activity implements FragmentManager.OnBackStackChangedListener {
+public class LearningActivity extends Activity implements FragmentManager.OnBackStackChangedListener, OnInitListener {
 
     /**
      * A handler object, used for deferring UI operations.
@@ -63,6 +68,11 @@ public class LearningActivity extends Activity implements FragmentManager.OnBack
     private static int currentWord;
     private CardFrontFragment frontCard;
     private CardBackFragment backCard;
+
+    private final int MY_DATA_CHECK_CODE = 0;
+
+    private static TextToSpeech myTTS;
+    private final Locale SPANISH = new Locale("es", "ES");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -112,6 +122,59 @@ public class LearningActivity extends Activity implements FragmentManager.OnBack
                 flipCard();
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Intialize the TTS engine
+        if (myTTS == null) {
+            Intent checkTTSIntent = new Intent();
+            checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+            startActivityForResult(checkTTSIntent, MY_DATA_CHECK_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == MY_DATA_CHECK_CODE) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                myTTS = new TextToSpeech(this, this);
+            }
+            else {
+                Intent installTTSIntent = new Intent();
+                installTTSIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installTTSIntent);
+            }
+        }
+    }
+
+    @Override
+    public void onInit(int initStatus) {
+        if (initStatus == TextToSpeech.SUCCESS) {
+            if ("en".equals(lesson.getLanguage())) {
+                if (myTTS.isLanguageAvailable(Locale.UK) == TextToSpeech.LANG_AVAILABLE) {
+                    myTTS.setLanguage(Locale.UK);
+                } else {
+                    myTTS.setLanguage(Locale.US);
+                }
+            } else if ("es".equals(lesson.getLanguage())) {
+                // if (myTTS.isLanguageAvailable(SPANISH) == TextToSpeech.LANG_AVAILABLE) {
+                myTTS.setLanguage(SPANISH);
+                // } else {
+                // myTTS.setLanguage(Locale.US);
+                // }
+            } else if ("fr".equals(lesson.getLanguage())) {
+                if (myTTS.isLanguageAvailable(Locale.FRENCH) == TextToSpeech.LANG_AVAILABLE) {
+                    myTTS.setLanguage(Locale.FRENCH);
+                } else {
+                    myTTS.setLanguage(Locale.US);
+                }
+            }
+        } else if (initStatus == TextToSpeech.ERROR) {
+            Toast.makeText(this, "Sorry! Text To Speech failed...", Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -299,11 +362,37 @@ public class LearningActivity extends Activity implements FragmentManager.OnBack
             super.onActivityCreated(savedInstanceState);
 
             updateView();
+            attachListeners();
         }
 
         public void updateView() {
             TextView correctTranslation = (TextView) getActivity().findViewById(R.id.learning_correctTranslation);
             correctTranslation.setText(words.get(currentWord).getCorrectTranslation());
+        }
+
+        public void attachListeners() {
+            ImageButton speakButton = (ImageButton) getActivity().findViewById(R.id.learning_speakTranslation);
+            speakButton.setOnClickListener(new OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    speakWords(words.get(currentWord).getCorrectTranslation());
+                }
+            });
+        }
+    }
+
+    private static void speakWords(String words) {
+        myTTS.speak(words, TextToSpeech.QUEUE_ADD, null);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (myTTS != null) {
+            myTTS.shutdown();
+            myTTS = null;
         }
     }
 }
