8941b9602e8ac34169428885536d0beb677523da
Albert Bihler <bihleral@yahoo.com>
2014-09-15 19:57:30 +0200

    App Info OK Button

--- src/de/albert/bihler/andrvoc/AppInfoActivity.java
+++ src/de/albert/bihler/andrvoc/AppInfoActivity.java
@@ -1,7 +1,14 @@
 package de.albert.bihler.andrvoc;
 
+import java.util.Collections;
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import de.albert.bihler.andrvoc.db.TrainingLogDataSource;
+import de.albert.bihler.andrvoc.model.Vokabel;
 import de.albert.bihler.andrvoc.orangeiron.R;
 
 public class AppInfoActivity extends Activity {
@@ -28,6 +35,11 @@ public class AppInfoActivity extends Activity {
     protected void onStop() {
         super.onStop();
 
+    }
+
+    /** Called when the user clicks the OK button */
+    public void buttonOK(View view) {
+        this.finish();
     }
 
 }
